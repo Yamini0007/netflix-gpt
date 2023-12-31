1926698ad11bdd216385aeebc0841447ee280e6d
Yamini0007 <chitikela.yamini@gmail.com>
2023-12-31 19:25:13 +0530

    Change in Title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="text-2xl font-bold text-blue-800">
-     Namaste user, Let's build Netflix GPT
+     Namaste user, Let's build Netflix GPT!!
     </div>
   );
 }
