349b36cc42a2605002800e94ed27936910cbeb3b
Yamini0007 <chitikela.yamini@gmail.com>
2023-12-31 19:09:14 +0530

    Title updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-green-800">
-     Namaste React, Let's build Netflix GPT
+     Namaste user, Let's build Netflix GPT
     </div>
   );
 }
