7c3cb4a7b28d58ea168230779cd574bdd741c157
Yamini0007 <chitikela.yamini@gmail.com>
2023-12-31 19:14:14 +0530

    Updated style

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
+    <div className="text-2xl font-bold text-blue-800">
      Namaste user, Let's build Netflix GPT
     </div>
   );
